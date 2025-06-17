bf5d645e019698ff128a11aaa8f1218960ffcc87
Oscar D Jarquin Perez <115858753+oscar-jp@users.noreply.github.com>
2025-06-16 20:29:38 -0500

    feat: add marketplace tables

---
+++ database/migrations/2025_06_17_012808_create_deliveries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders');
+            $table->foreignId('courier_id')->constrained('users');
+            $table->enum('status', ['pending', 'assigned', 'picked_up', 'delivered', 'cancelled'])
+                ->default('pending');
+            $table->timestamp('delivered_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
